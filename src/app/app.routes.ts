17642ce755076d4292945bd0fa0c5226667021a4
SAAD <moussaifsaad4@gmail.com>
2026-04-13 18:28:34 +0100

    edit authervice and setting Security

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/guard/auth.guard';
 import { adminGuard } from './core/guard/admin.guard';
 
 export const routes: Routes = [
@@ -23,21 +22,19 @@ export const routes: Routes = [
   },
   {
     path: 'cart',
-    canActivate: [authGuard],
     loadComponent: () =>
       import('./features/cart/cart.component')
         .then(m => m.CartComponent)
   },
   {
     path: 'checkout',
-    canActivate: [authGuard],
     loadComponent: () =>
       import('./features/checkout/checkout.component')
         .then(m => m.CheckoutComponent)
   },
   {
     path: 'admin',
-    canActivate: [authGuard, adminGuard],
+    canActivate: [adminGuard],
     loadChildren: () =>
       import('./features/admin/admin.routes')
         .then(m => m.adminRoutes)
--- src/app/core/guard/admin.guard.ts
+++ src/app/core/guard/admin.guard.ts
@@ -1,15 +1,14 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const adminGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
-  const router = inject(Router);
 
-  if (auth.isAdmin()) {
+  if (auth.isLoggedIn() && auth.isAdmin()) {
     return true;
   }
 
-  router.navigate(['/']);
+  auth.login();
   return false;
 };
